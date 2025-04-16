0cd051ecec1ab6351d5b3ceb7087c5c636ef5070
aPannell064 <adamkpannell@gmail.com>
2025-04-16 11:54:41 -0600

    Dynamic

--- code.js
+++ code.js
@@ -1,23 +1,27 @@
 function tsp_hk(distance_matrix) {
     if(distance_matrix.length < 2) {return 0;}
-    var path;
+    var cache = [];
+
+    function heldKarp(cities, start) {
+        if(cities.length == 2) {return cities[0][1];}
+        var key = JSON.stringify(cities);
+        if(cache[key] === undefined) {cache[key] = [];}
+        if(cache[key][start] !== undefined) {return cache[key][start];}
+    
+        var min = Infinity;
+        var trimmed = graphTrim(cities, start);
+        for(var i = 0; i < trimmed.length; i++) {
+            min = Math.min(min, 
+                heldKarp(trimmed, i) + cities[start][i < start ? i : i+1]);
+        }
+        cache[key][start] = min;
+        return min;
+    }
+
     var min = Infinity;
     for(var city in distance_matrix) {
-        path = heldKarp(distance_matrix, city);
-        if(path < min) {min = path};
+        min = Math.min(min, heldKarp(distance_matrix, city));
     }
-    return min;
-}
-
-function heldKarp(cities, start) {
-    if(cities.length == 2) {return cities[0][1];}
-    var path;
-    var min = Infinity;
-    var trimmed = graphTrim(cities, start)
-    for(var i = 0; i < trimmed.length; i++) {
-        path = heldKarp(trimmed, i) + cities[start][i < start ? i : i+1];
-        if(path < min) {min = path};
-    } 
     return min;
 }
 
