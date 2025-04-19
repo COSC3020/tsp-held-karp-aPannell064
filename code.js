034cd44067424e88fd613b21a0e3e118f7ae38e7
aPannell064 <adamkpannell@gmail.com>
2025-04-18 23:18:04 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,22 +1,28 @@
 function tsp_hk(distance_matrix) {
     if(distance_matrix.length < 2) {return 0;}
     var cache = [];
+    var cities = [];
+    for (var i = 0; i < distance_matrix.length; i++) {
+        cities.push(i);
+    }
 
     function heldKarp(cities, start) {
         //if |cities| == 2: return length between those two cities
-        if(cities.length == 2) {return cities[0][1];}
+        if(cities.length == 2) {return distance_matrix[cities[0]][cities[1]];}
 
-        //Check cache for to see if this subset has been processed before
-        var key = JSON.stringify(cities);
+        //Check cache to see if this subset has been processed before
+        var key = JSON.stringify(cities.sort());
         if(cache[key] === undefined) {cache[key] = [];}
         if(cache[key][start] !== undefined) {return cache[key][start];}
-    
+ 
         //Get minimum for held-karp of the rest of the graph
         var min = Infinity;
-        var trimmed = graphTrim(cities, start);
+        var trimmed = [...cities];
+        trimmed.splice(trimmed.indexOf(start), 1);
         for(var i = 0; i < trimmed.length; i++) {
+            var city = trimmed[i];
             min = Math.min(min, 
-                heldKarp(trimmed, i) + cities[start][i < start ? i : i+1]);
+                heldKarp(trimmed, city) + distance_matrix[start][city]);
         }
         cache[key][start] = min;
         return min;
@@ -24,18 +30,8 @@ function tsp_hk(distance_matrix) {
 
     //|V| different start positions at beginning
     var min = Infinity;
-    for(var city in distance_matrix) {
-        min = Math.min(min, heldKarp(distance_matrix, city));
+    for(var i = 0; i < cities.length; i++) {
+        min = Math.min(min, heldKarp(cities, cities[i]));
     }
     return min;
-}
-
-//Returns a trimmed graph without a specific vertex
-function graphTrim(graph, vertex) {
-    var copy = JSON.parse(JSON.stringify(graph));
-    copy.splice(vertex, 1);
-    for(var i = 0; i < copy.length; i++) {
-        copy[i].splice(vertex, 1);
-    }
-    return copy;
 }
